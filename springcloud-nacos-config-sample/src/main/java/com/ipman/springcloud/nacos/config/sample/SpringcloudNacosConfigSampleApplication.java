9882a6c1ac6c76952739f3acece6ff01827402e4
ipipman <ipipman@163.com>
2020-12-30 12:18:32 +0800

    add readme in spring cloud nacos sample

--- springcloud-nacos-config-sample/src/main/java/com/ipman/springcloud/nacos/config/sample/SpringcloudNacosConfigSampleApplication.java
+++ springcloud-nacos-config-sample/src/main/java/com/ipman/springcloud/nacos/config/sample/SpringcloudNacosConfigSampleApplication.java
@@ -5,11 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableDiscoveryClient
+@EnableDiscoveryClient //开启后向Nocas注册中心注册当前实例
 public class SpringcloudNacosConfigSampleApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringcloudNacosConfigSampleApplication.class, args);
 	}
-
 }
