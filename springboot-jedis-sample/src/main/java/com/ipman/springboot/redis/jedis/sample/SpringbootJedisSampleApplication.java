d27dfec4ca9059dbbc7e4aaeace782ac9b450bb4
ipipman <ipipman@163.com>
2021-01-05 18:56:26 +0800

    add jedis sample in springboot

---
+++ springboot-jedis-sample/src/main/java/com/ipman/springboot/redis/jedis/sample/SpringbootJedisSampleApplication.java
@@ -0,0 +1,13 @@
+package com.ipman.springboot.redis.jedis.sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootJedisSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootJedisSampleApplication.class, args);
+	}
+
+}
---
+++ springboot-jedis-sample/src/test/java/com/ipman/springboot/redis/jedis/sample/SpringbootJedisSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ipman.springboot.redis.jedis.sample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootJedisSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
