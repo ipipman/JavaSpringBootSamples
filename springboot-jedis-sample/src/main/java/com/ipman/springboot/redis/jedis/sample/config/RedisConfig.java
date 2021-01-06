fa17d85a4721e8ad2d1633b0f27a5a18a51448c8
ipipman <ipipman@163.com>
2021-01-06 22:42:23 +0800

    add jedis sample

---
+++ springboot-jedis-sample/src/main/java/com/ipman/springboot/redis/jedis/sample/config/RedisConfig.java
@@ -0,0 +1,52 @@
+package com.ipman.springboot.redis.jedis.sample.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.JedisPoolConfig;
+
+/**
+ * Created by ipipman on 2021/1/6.
+ *
+ * @version V1.0
+ * @Package com.ipman.springboot.redis.jedis.sample.config
+ * @Description: (用一句话描述该文件做什么)
+ * @date 2021/1/6 10:11 下午
+ */
+@Configuration
+public class RedisConfig {
+
+    @Value("${spring.redis.host}")
+    private String host;
+
+    @Value("${spring.redis.port}")
+    private int port;
+
+    @Value("${spring.redis.timeout}")
+    private int timeout;
+
+    @Value("${spring.redis.jedis.pool.max-idle}")
+    private int maxIdle;
+
+    @Value("${spring.redis.jedis.pool.max-wait}")
+    private long maxWaitMillis;
+
+    @Value("${spring.redis.password}")
+    private String password;
+
+    @Value("${spring.redis.block-when-exhausted}")
+    private boolean  blockWhenExhausted;
+
+    @Bean
+    public JedisPool redisPoolFactory()  throws Exception{
+        JedisPoolConfig jedisPoolConfig = new JedisPoolConfig();
+        jedisPoolConfig.setMaxIdle(maxIdle);
+        jedisPoolConfig.setMaxWaitMillis(maxWaitMillis);
+        // 连接耗尽时是否阻塞, false报异常,ture阻塞直到超时, 默认true
+        jedisPoolConfig.setBlockWhenExhausted(blockWhenExhausted);
+        // 是否启用pool的jmx管理功能, 默认true
+        jedisPoolConfig.setJmxEnabled(true);
+        return new JedisPool(jedisPoolConfig, host, port, timeout, password);
+    }
+}
---
+++ springboot-jedis-sample/src/test/java/com/ipman/springboot/redis/jedis/sample/JedisTest.java
@@ -0,0 +1,17 @@
+package com.ipman.springboot.redis.jedis.sample;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * Created by ipipman on 2021/1/6.
+ *
+ * @version V1.0
+ * @Package com.ipman.springboot.redis.jedis.sample
+ * @Description: (用一句话描述该文件做什么)
+ * @date 2021/1/6 10:00 上午
+ */
+@SpringBootTest
+public class JedisTest {
+
+
+}
