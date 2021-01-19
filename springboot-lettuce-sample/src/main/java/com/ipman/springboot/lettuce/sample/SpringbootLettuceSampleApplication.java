83c6f39e6ab41dda8230f75fff2dc1a4b68fa419
ipipman <ipipman@163.com>
2021-01-19 22:28:44 +0800

    add lettuce sample in springboot

---
+++ springboot-lettuce-sample/src/main/java/com/ipman/springboot/lettuce/sample/SpringbootLettuceSampleApplication.java
@@ -0,0 +1,13 @@
+package com.ipman.springboot.lettuce.sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootLettuceSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootLettuceSampleApplication.class, args);
+	}
+
+}
---
+++ springboot-lettuce-sample/src/test/java/com/ipman/springboot/lettuce/sample/SpringbootLettuceSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ipman.springboot.lettuce.sample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootLettuceSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
