09614cda89cb6e215d3a4923f55c2e877d8b2aca
ipipman <ipipman@163.com>
2021-01-27 20:00:39 +0800

    add  redisson sample in springboot framework

---
+++ springboot-redisson-sample/src/main/java/com/ipman/springboot/redisson/sample/SpringbootRedissonSampleApplication.java
@@ -0,0 +1,13 @@
+package com.ipman.springboot.redisson.sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootRedissonSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootRedissonSampleApplication.class, args);
+	}
+
+}
---
+++ springboot-redisson-sample/src/test/java/com/ipman/springboot/redisson/sample/SpringbootRedissonSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ipman.springboot.redisson.sample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootRedissonSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
