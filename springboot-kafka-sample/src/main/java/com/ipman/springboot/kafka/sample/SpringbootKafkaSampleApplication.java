2a15e2ac814cbbb922010e10e81ae4ff475b1fb8
ipipman <ipipman@163.com>
2021-02-09 17:30:29 +0800

    add kafka sample in springboot fx

---
+++ springboot-kafka-sample/src/main/java/com/ipman/springboot/kafka/sample/SpringbootKafkaSampleApplication.java
@@ -0,0 +1,13 @@
+package com.ipman.springboot.kafka.sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootKafkaSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootKafkaSampleApplication.class, args);
+	}
+
+}
---
+++ springboot-kafka-sample/src/test/java/com/ipman/springboot/kafka/sample/SpringbootKafkaSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ipman.springboot.kafka.sample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootKafkaSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
