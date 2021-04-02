cb97c728e8d94391d42e7bb2c7b7a3dc72c69f7f
ipipman <ipipman@163.com>
2021-04-02 10:50:20 +0800

    add spring framework in  transactional sample

---
+++ springboot-transactional-sample/src/main/java/com/ipman/springboot/transactional/sample/SpringbootTransactionalSampleApplication.java
@@ -0,0 +1,13 @@
+package com.ipman.springboot.transactional.sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootTransactionalSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootTransactionalSampleApplication.class, args);
+	}
+
+}
---
+++ springboot-transactional-sample/src/test/java/com/ipman/springboot/transactional/sample/SpringbootTransactionalSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ipman.springboot.transactional.sample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootTransactionalSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
