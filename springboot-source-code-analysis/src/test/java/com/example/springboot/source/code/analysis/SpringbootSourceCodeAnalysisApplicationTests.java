d5cc691bcb9dd06638fdd1e3e279e022da5d990b
ipipman <ipipman@163.com>
2021-06-05 20:13:35 +0800

    add springboot start the process markdown

--- springboot-source-code-analysis/src/test/java/com/example/springboot/source/code/analysis/SpringbootSourceCodeAnalysisApplicationTests.java
+++ springboot-source-code-analysis/src/test/java/com/example/springboot/source/code/analysis/SpringbootSourceCodeAnalysisApplicationTests.java
@@ -1,6 +1,6 @@
 package com.example.springboot.source.code.analysis;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
