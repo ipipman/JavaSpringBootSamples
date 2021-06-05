5b9e0778ed14bd9f719ee7793d5b484c67605eed
ipipman <ipipman@163.com>
2021-06-05 18:28:18 +0800

    add springboot 2.x source code analysis project

---
+++ springboot-source-code-analysis/src/main/java/com/example/springboot/source/code/analysis/SpringbootSourceCodeAnalysisApplication.java
@@ -0,0 +1,13 @@
+package com.example.springboot.source.code.analysis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootSourceCodeAnalysisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootSourceCodeAnalysisApplication.class, args);
+    }
+
+}
---
+++ springboot-source-code-analysis/src/test/java/com/example/springboot/source/code/analysis/SpringbootSourceCodeAnalysisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springboot.source.code.analysis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootSourceCodeAnalysisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
