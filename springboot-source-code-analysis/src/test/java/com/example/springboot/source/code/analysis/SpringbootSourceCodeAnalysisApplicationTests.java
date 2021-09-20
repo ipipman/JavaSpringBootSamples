19454eec15bdfd38e6148f96d4e96c62155abcdf
ipipman <ipipman@163.com>
2021-09-20 20:13:22 +0800

    add springboot event listener source code analysis

--- springboot-source-code-analysis/src/test/java/com/example/springboot/source/code/analysis/SpringbootSourceCodeAnalysisApplicationTests.java
+++ springboot-source-code-analysis/src/test/java/com/example/springboot/source/code/analysis/SpringbootSourceCodeAnalysisApplicationTests.java
@@ -2,7 +2,7 @@ package com.example.springboot.source.code.analysis;
 
 import com.example.springboot.source.code.analysis.event.RainListener;
 import com.example.springboot.source.code.analysis.event.WeatherRunListener;
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
