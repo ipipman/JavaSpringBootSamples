385c4aea45946aac138ab0ff9cdb6acab30c11a5
ipipman <ipipman@163.com>
2024-03-03 20:46:04 +0800

    test account

--- springcloud-consul-config-sample/src/main/java/com/ipman/springcloud/consul/config/center/sample/SpringcloudConsulConfigSampleApplication.java
+++ springcloud-consul-config-sample/src/main/java/com/ipman/springcloud/consul/config/center/sample/SpringcloudConsulConfigSampleApplication.java
@@ -8,6 +8,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @EnableDiscoveryClient //让注册中心进行服务发现，将服务注册到服务组件上
 public class SpringcloudConsulConfigSampleApplication {
 
+	// hello
 	public static void main(String[] args) {
 		SpringApplication.run(SpringcloudConsulConfigSampleApplication.class, args);
 	}
